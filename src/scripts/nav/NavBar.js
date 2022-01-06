bab61973b5912707810d9b161774156160376180
nathanelmer <nelmer12@gmail.com>
2022-01-06 11:34:47 -0600

    fixed nav

--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -1,16 +1,29 @@
 import { renderApp } from "../main.js"
 import { Messages } from "../friends/DirectMessage.js"
+import { SendMsg } from "../message/MessageForm.js"
+import { GiffyGram } from "../GiffyGram.js"
+import { LoginForm } from "../auth/Login.js"
 
 
 export const NavBar = () => {
+    document.addEventListener("click", clickEvent => {
+        if (clickEvent.target.id === "messages") {
+           renderApp(Messages())
+        }
+        if (clickEvent.target.id === "sendMsg"){
+            renderApp(SendMsg())
+        }
+        if (clickEvent.target.id === "homepage"){
+            renderApp(GiffyGram())
+        }
+        if (clickEvent.target.id === "logout"){
+            localStorage.removeItem("gg_user")
+            renderApp(LoginForm())
+        }
+    })
     return `
-    <section class="notification__count">Messages</section>
-    <section class="navigation__message">Send Msg</section>
-    <section class="navigation__logout">Logout</section>
+    <section class="notification__count" id="messages">Messages</section>
+    <section class="navigation__message" id="sendMsg">Send Msg</section>
+    <section class="navigation__logout" id="logout">Logout</section>
     `
 }
-// document.addEventListener("click", clickEvent => {
-//     if (clickEvent.target.id === "dsfgsdfg") {
-//        renderApp(Messages())
-//     }
-// })
